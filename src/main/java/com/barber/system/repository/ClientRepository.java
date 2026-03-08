b8224c184e6a6d47bcba15dae1d2587a26d895f5
Younes Adir <younesadir@gmail.com>
2026-03-08 14:41:36 +0000

    Fix missing List imports in repositories causing build failure

--- src/main/java/com/barber/system/repository/ClientRepository.java
+++ src/main/java/com/barber/system/repository/ClientRepository.java
@@ -5,6 +5,7 @@ import com.barber.system.model.BarberShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
--- src/main/java/com/barber/system/repository/ServiceRepository.java
+++ src/main/java/com/barber/system/repository/ServiceRepository.java
@@ -5,6 +5,8 @@ import com.barber.system.model.BarberShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
     List<Service> findByBarberShop(BarberShop barberShop);
